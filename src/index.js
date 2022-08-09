4c879cc7ea38f34b034e721154ceac7e8ceb517d
phuong1810 <ntquephuong.nina@gmail.com>
2022-08-09 14:53:30 +0700

    :sparkles: Router nodejs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { engine } = require('express-handlebars')
 const path = require('path')
 const app = express()
 const port = 3000
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,24 +18,10 @@ app.engine('.hbs', engine({
 app.set('view engine', 'hbs')
 app.set('views',path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+//Routers init
+route(app)
 
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.get('/search', (req, res) => {
-  console.log(req.query.q)
-  res.render('search')
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body.q)
-  res.send('')
-})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
