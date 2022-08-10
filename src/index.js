9cec95d9af98d5dcd8926f6c97d8c06840883b98
phuong1810 <ntquephuong.nina@gmail.com>
2022-08-10 09:10:32 +0700

    :tada: Phuong commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const path = require('path')
 const app = express()
 const port = 3000
 const route = require('./routes')
+const db = require('./config/db')
+
+//conect db
+db.connect()
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,12 +20,12 @@ app.engine('.hbs', engine({
   extname:'.hbs'
 }))
 app.set('view engine', 'hbs')
-app.set('views',path.join(__dirname, 'resources/views'))
+app.set('views',path.join(__dirname, 'resources','views'))
 
 //Routers init
 route(app)
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`App listening on port http://localhost:${port}`)
 })
